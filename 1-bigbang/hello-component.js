fd7cf25e55e714b349146a10e1b95797837feab6
Danillo Corvalan <danillo.corvalan@gmail.com>
2014-05-11 12:51:37 -0300

    Nested Components

--- 1-bigbang/hello-component.js
+++
@@ -1,9 +0,0 @@
-/** @jsx React.DOM */
-
-var HelloMessage = React.createClass({
-  render: function () {
-    return <div>Hello TDC!</div>;
-  }
-});
-
-React.renderComponent(<HelloMessage />, document.getElementById('main'));
