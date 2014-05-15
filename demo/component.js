7d3be3a023972ef8a12109750290b668a2bda8b1
Danillo Corvalan <danillo.corvalan@gmail.com>
2014-05-15 00:59:30 -0300

    last step demo

--- demo/component.js
+++ demo/component.js
@@ -1,1 +1,41 @@
 /** @jsx React.DOM */
+
+var MainPage = React.createClass({
+  getInitialState: function () {
+    return { trilhas: [], trilha: '' };
+  },
+  handleAdd: function () {
+    var trilhas = this.state.trilhas.concat([this.state.trilha]);
+    this.setState({ trilhas: trilhas, trilha: ''});
+  },
+  handleChange: function (e) {
+    this.setState({trilha: e.target.value});
+  },
+  render: function () {
+    return (
+      <div>
+        <input type="text" value={this.state.trilha} onChange={this.handleChange} />
+        <button onClick={this.handleAdd}>Adicionar</button>
+        <TrilhaList trilhas={this.state.trilhas} />
+      </div>
+    );
+  }
+});
+
+var TrilhaList = React.createClass({
+  render: function () {
+    var renderTrilha = function (trilha) {
+      return <li>Hello Trilha <strong>{trilha}</strong></li>;
+    };
+
+    return (
+      <ul>
+        {this.props.trilhas.map(renderTrilha)}
+      </ul>
+    );
+  }
+});
+
+React.renderComponent(
+  <MainPage />,
+  document.getElementById('main'));
